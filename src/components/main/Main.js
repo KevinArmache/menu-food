7f4e642333625d4edfd4be7a4c1bc867ce1ed64a
KevinArmache <kevinarmache@gmail.com>
2023-01-22 13:37:38 +0100

    add: edit json file food

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,7 +7,7 @@ function Main() {
     <div>
       <div className="container-main">
         <div className="main-title">
-          <span>Nom du restaurant</span>
+          <span>Air Bridge</span>
           <span>Menu</span>
         </div>
       </div>
